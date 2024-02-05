e230995cbf3c904a682d556692164512986b96d8
uslipe <felipe.pereira.nave@gmail.com>
2024-02-04 22:15:29 -0300

    Run configurations javafx + 1ª tela

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class App extends Application{
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/application/telaLogin.fxml"));
+        primaryStage.setTitle("Primeira Tela");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
 }
--- src/negocio/ControladorDeUsuario.java
+++ src/negocio/ControladorDeUsuario.java
@@ -5,60 +5,61 @@ import dados.RepositorioUsuario;
 import negocio.beans.Usuario;
 
 public class ControladorDeUsuario {
-	    private Repositorio<Usuario> repositorioUsuario;
+	private Repositorio<Usuario> repositorioUsuario;
 
-	    public ControladorDeUsuario(Repositorio<Usuario> repositorioUsuario2) {
-			// TODO Auto-generated constructor stub
-		}
-
-		public void ControladorUsuario(Repositorio<Usuario> repositorioUsuario) {
-	        this.repositorioUsuario = repositorioUsuario;
-	    }
-
-	    // Metodos CRUD p/ Usuarios
-	    public void criarUsuario(String nome, String username, String senha, int idade) {
-	        Usuario novoUsuario = new Usuario(nome, username, senha, idade);
-	        repositorioUsuario.adicionar(novoUsuario);
-	        System.out.println("Usuario criado com sucesso!");
-	    }
-
-	    public void editarUsuario(Usuario usuario, String novoNome, String novoNomeUsuario, String novaSenha, int novaIdade) {
-	    	if (repositorioUsuario.listarTodos().contains(usuario)) {
-	    		usuario.setNome(novoNome);
-	    		usuario.setNomeUsuario(novoNomeUsuario);
-	    		usuario.setSenha(novaSenha);
-	    		usuario.setIdade(novaIdade);
-	            System.out.println("Usuario editado com sucesso.");
-	        } else {
-	            System.out.println("O usuario nao existe.");
-	        }
-	    }
-
-	    public void excluirUsuario(Usuario usuario) {
-	        if (repositorioUsuario.listarTodos().contains(usuario)) {
-	            repositorioUsuario.remover(usuario);
-	            System.out.println("Usuario excluido com sucesso!");
-	        } else {
-	            System.out.println("O usuario nao existe.");
-	        }
-	    }
-
-	    // Metodos extra p/ usuarios
-	    public void seguirUsuario(Usuario seguidor, Usuario usuarioASeguir) {
-	        if (!seguidor.equals(usuarioASeguir) && !seguidor.getSeguindo().contains(usuarioASeguir)) {
-	            seguidor.seguir(usuarioASeguir);
-	            System.out.println(seguidor.getNome() + " comecou a seguir " + usuarioASeguir.getNome());
-	        } else {
-	            System.out.println("Nao foi possivel seguir o usuario.");
-	        }
-	    }
-
-	    public void desseguirUsuario(Usuario usuarioAlice, Usuario usuarioBob) {
-	        if (usuarioAlice.getSeguindo().contains(usuarioBob)) {
-	            usuarioAlice.desseguir(usuarioBob);
-	            System.out.println(usuarioAlice.getNome() + " deixou de seguir " + usuarioBob.getNome());
-	        } else {
-	            System.out.println("Nao foi possivel deixar de seguir o usuario.");
-	        }
-	    }
+	public ControladorDeUsuario(Repositorio<Usuario> repositorioUsuario2) {
+		// TODO Auto-generated constructor stub
 	}
+
+	public void ControladorUsuario(Repositorio<Usuario> repositorioUsuario) {
+		this.repositorioUsuario = repositorioUsuario;
+	}
+
+	// Metodos CRUD p/ Usuarios
+	public void criarUsuario(String nome, String username, String senha, int idade) {
+		Usuario novoUsuario = new Usuario(nome, username, senha, idade);
+		repositorioUsuario.adicionar(novoUsuario);
+		System.out.println("Usuario criado com sucesso!");
+	}
+
+	public void editarUsuario(Usuario usuario, String novoNome, String novoNomeUsuario, String novaSenha,
+			int novaIdade) {
+		if (repositorioUsuario.listarTodos().contains(usuario)) {
+			usuario.setNome(novoNome);
+			usuario.setNomeUsuario(novoNomeUsuario);
+			usuario.setSenha(novaSenha);
+			usuario.setIdade(novaIdade);
+			System.out.println("Usuario editado com sucesso.");
+		} else {
+			System.out.println("O usuario nao existe.");
+		}
+	}
+
+	public void excluirUsuario(Usuario usuario) {
+		if (repositorioUsuario.listarTodos().contains(usuario)) {
+			repositorioUsuario.remover(usuario);
+			System.out.println("Usuario excluido com sucesso!");
+		} else {
+			System.out.println("O usuario nao existe.");
+		}
+	}
+
+	// Metodos extra p/ usuarios
+	public void seguirUsuario(Usuario seguidor, Usuario usuarioASeguir) {
+		if (!seguidor.equals(usuarioASeguir) && !seguidor.getSeguindo().contains(usuarioASeguir)) {
+			seguidor.seguir(usuarioASeguir);
+			System.out.println(seguidor.getNome() + " comecou a seguir " + usuarioASeguir.getNome());
+		} else {
+			System.out.println("Nao foi possivel seguir o usuario.");
+		}
+	}
+
+	public void desseguirUsuario(Usuario usuarioAlice, Usuario usuarioBob) {
+		if (usuarioAlice.getSeguindo().contains(usuarioBob)) {
+			usuarioAlice.desseguir(usuarioBob);
+			System.out.println(usuarioAlice.getNome() + " deixou de seguir " + usuarioBob.getNome());
+		} else {
+			System.out.println("Nao foi possivel deixar de seguir o usuario.");
+		}
+	}
+}
